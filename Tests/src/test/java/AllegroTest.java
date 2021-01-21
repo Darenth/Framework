4915a7d0658ef97d556efb45ef41f4c06b9b3ca0
Dimka Harb <darenth1@gmail.com>
2021-01-21 14:34:30 +0100

    change name UnitTest.java to AllegroTest

--- Tests/src/test/java/AllegroTest.java
+++ Tests/src/test/java/AllegroTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
-public class UnitTest {
+public class AllegroTest {
 
     WebDriver driver;
     WebDriverWait wait;
